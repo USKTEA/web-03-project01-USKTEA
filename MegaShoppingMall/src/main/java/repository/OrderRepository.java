256efd833478fc91aab864b3f89c8f73fae72742
usktea <tjrxo1234@gmail.com>
2022-09-21 15:07:11 +0900

    OrderRepository에 캐싱 추가

--- MegaShoppingMall/src/main/java/repository/OrderRepository.java
+++ MegaShoppingMall/src/main/java/repository/OrderRepository.java
@@ -4,20 +4,26 @@ import models.FileManager;
 import models.Order;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class OrderRepository {
     private FileManager fileManager;
+    private List<Order> orderList = new ArrayList<>();
 
     OrderRepository() {
         fileManager = new FileManager("orders.csv");
     }
 
     public void record(Order order) throws IOException {
+        orderList.add(order);
+
         fileManager.storeReceipt(order);
     }
 
     public List<Order> getOrders() {
-        return fileManager.getReceipts();
+        orderList = fileManager.getReceipts();
+
+        return orderList;
     }
 }
